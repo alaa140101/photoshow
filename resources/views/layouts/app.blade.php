9b6134a25a13ccb3998b8fd45153ebd381b2bec3
alaa140101 <alaa1400@hotmail.com>
2019-05-12 08:59:00 +0300

    Cover image upload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,10 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.5.3/css/foundation-float.css">
 </head>
 <body>
+  @include('inc.topbar')
+
   <div class="row">
+      @include('inc.messages')
       @yield('content')
   </div>
 </body>
